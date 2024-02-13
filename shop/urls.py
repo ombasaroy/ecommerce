89a4e3ea2a252f4d5bd7a53e69421cada8c308b1
ombasaroy <ombasaroy@gmail.comgit config --global user.name ombasaroygit config --global user.email ombasaroy@gmail.com>
2024-02-13 19:16:10 +0300

    Login, logout fixed

--- shop/urls.py
+++ shop/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('login', views.loginUser, name='login'),
+    path('logout', views.logoutUser, name='logout')
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
@@ -10,3 +12,30 @@ def index(request):
     context = {'products': products}
 
     return render(request, 'index.html', context)
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def loginUser(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have  been logged in')
+            return redirect('index')
+        else:
+            messages.error(request, 'Login failed. Incorrect username or password')
+            return redirect('login')
+    return render(request, 'login.html')
+
+
+def logoutUser(request):
+    logout(request)
+    messages.success(request, 'Logged out successful')
+    return redirect('login')
