0a9252323cc9d61ad69145e3d0b44369fceedfeb
ombasaroy <ombasaroy@gmail.comgit config --global user.name ombasaroygit config --global user.email ombasaroy@gmail.com>
2024-02-13 11:55:49 +0300

    Display Products

---
+++ shop/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Product
+
+# Create your views here.
+
+
+def index(request):
+    products = Product.objects.all()
+
+    context = {'products': products}
+
+    return render(request, 'index.html', context)
